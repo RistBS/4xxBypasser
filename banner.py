db3069bcd82a7c689f2cca5159f14142343c699f
RistBS <75935486+RistBS@users.noreply.github.com>
2021-07-18 21:33:14 +0200

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,12 @@
+def banner():
+    print("""
+    \033[91m
+     ___         _____                             
+    | | |_ _ _ _| __  |_ _ ___ ___ ___ ___ ___ ___ 
+    |_  |_'_|_'_| __ -| | | . | .'|_ -|_ -| -_|  _|
+      |_|_,_|_,_|_____|_  |  _|__,|___|___|___|_|  
+                      |___|_|       
+    \033[0m
+    \033[92m[+]\033[0m \033[28mCreatpr : \033[4mRistBS\033[0m
+    \033[93m[?]\033[0m \033[28mInfo : this tool was created to bypass error responses on the client side (4xx) \033[0m
+    \033[91m[!]\033[0m \033[28mUse this tool within a legal framework !\033[0m\n\n""")
