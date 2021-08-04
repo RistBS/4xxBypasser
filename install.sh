960d1f8a3479438360bb201622a9c12df2104056
RistBS <75935486+RistBS@users.noreply.github.com>
2021-08-04 23:28:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "installing pip..."
-apt install python-pip
+apt install python3-pip
 echo "installing libs..."
 pip3 install -r requirements.txt
 ln -sf $(pwd)/4xxbypasser.py /usr/local/bin/4xxbypasser
